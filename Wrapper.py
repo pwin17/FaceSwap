be672f5bfb099fd9e0f9b27cad136226d21f990e
Pyone Thant Win <pyone@macbook-pro.myfiosgateway.com>
2022-03-03 20:51:51 -0500

    4.2 a bit more

--- Wrapper.py
+++ Wrapper.py
@@ -49,9 +49,6 @@ def rect_contains(rect, point) :
         return False
     return True
 
-def draw_point(img, p, color ) :
-    cv2.circle( img, p, 2, color, cv2.cv.CV_FILLED, cv2.LINE_AA, 0 )
-
 # Draw delaunay triangles
 def draw_delaunay(img, subdiv, delaunay_color ) :
 
@@ -72,39 +69,64 @@ def draw_delaunay(img, subdiv, delaunay_color ) :
             cv2.line(img, pt3, pt1, delaunay_color, 1, cv2.LINE_AA, 0)
 
 
+## destination -- the celebrity image?
+## source -- main image (the image we want to overlay the celebrity image to) 
 # read image
-image = cv2.imread("./data/aim_face.jpeg")
-size = image.shape 
-img_original = image.copy()
+dest = cv2.imread("./data/celebrity.jpg")
+size = dest.shape 
+dest_original = dest.copy()
+source = cv2.imread("./data/test2.jpg")
+size = source.shape 
+source_original = source.copy()
+dest_original = dest.copy()
 # get face detection and facial landmarks
-rect_arr, shapes = get_facial_landmarks(image)
+source_rect, source_shapes = get_facial_landmarks(source)
+dest_rect, dest_shapes = get_facial_landmarks(dest)
 
-temp_img = image.copy()
-print("Total number of faces detected: ", len(rect_arr))
-for i in range(len(rect_arr)):
-    r = rect_arr[i]
+tempS = source.copy()
+tempD = dest.copy()
+print("Total number of faces detected: ", len(source_rect)+len(dest_rect))
+## Assuming for now that each image has same number of faces
+for i in range(len(source_rect)):
+    r = source_rect[i]
     start_point = r[0]
     end_point = r[1]
-    temp_img = cv2.rectangle(temp_img, start_point, end_point, (0,255,0), thickness=2)
-    for j, (x, y) in enumerate(shapes[i]):
+    tempS = cv2.rectangle(tempS, start_point, end_point, (0,255,0), thickness=2)
+    r = dest_rect[i]
+    start_point = r[0]
+    end_point = r[1]
+    tempD = cv2.rectangle(tempD, start_point, end_point, (0,255,0), thickness=2)
+    for j, (x, y) in enumerate(source_shapes[i]):
         # Draw the circle to mark the keypoint 
-        cv2.circle(temp_img, (x, y), 1, (0, 0, 255), 2)
-# cv2.imshow('face detection', temp_img)
-# if cv2.waitKey(0)==ord('q'):
-#     cv2.destroyAllWindows()
+        cv2.circle(tempS, (x, y), 1, (0, 0, 255), 2)
+    for j, (x, y) in enumerate(dest_shapes[i]):
+        # Draw the circle to mark the keypoint 
+        cv2.circle(tempD, (x, y), 1, (0, 0, 255), 2)
+cv2.imshow('face detection source', tempS)
+cv2.imshow('face detection destination', tempD)
+if cv2.waitKey(0)==ord('q'):
+    cv2.destroyAllWindows()
 
 # reshape as points for triangulation
-old_shape = np.shape(shapes)
-shapes_new = np.reshape(shapes, (old_shape[0]*old_shape[1], old_shape[2]))
-rect_size = (0, 0, size[1], size[0])
-subdiv  = cv2.Subdiv2D(rect_size)
-
-for p in shapes_new :
+old_shape = np.shape(source_shapes)
+source_shapes_new = np.reshape(source_shapes, (old_shape[0]*old_shape[1], old_shape[2]))
+source_size = (0, 0, size[1], size[0])
+subdiv  = cv2.Subdiv2D(source_size)
+for p in source_shapes_new:
     p = (p[0],p[1])
     subdiv.insert(p)
+draw_delaunay(source, subdiv, (255, 255, 255))
+old_shape = np.shape(dest_shapes)
+dest_shapes_new = np.reshape(dest_shapes, (old_shape[0]*old_shape[1], old_shape[2]))
+dest_size = (0, 0, size[1], size[0])
+subdiv  = cv2.Subdiv2D(dest_size)
+for p in dest_shapes_new:
+    p = (p[0],p[1])
+    subdiv.insert(p)
+draw_delaunay(dest, subdiv, (255, 255, 255))
 
-img_copy = img_original.copy()
-draw_delaunay(img_copy, subdiv, (255, 255, 255))
-cv2.imshow("Delaunay Triangulation", img_copy)
+
+cv2.imshow("Delaunay Triangulation", source)
+cv2.imshow("Delaunay Triangulation", dest)
 if cv2.waitKey(0)==ord('q'):
     cv2.destroyAllWindows()
